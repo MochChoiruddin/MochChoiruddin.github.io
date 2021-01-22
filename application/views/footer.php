0e6c66aaa7121417a72516f4db902237d9385878
MochChoiruddin <mochchoiruddin99@gmail.com>
2021-01-22 15:26:54 +0700

    first commit

---
+++ application/views/footer.php
@@ -0,0 +1,55 @@
+<footer class="ftco-footer ftco-bg-dark ftco-section">
+	<div class="container px-md-5">
+		<div class="row mb-5">
+			<div class="col-md">
+				<div class="ftco-footer-widget mb-4 ml-md-4">
+					<h2 class="ftco-heading-2">Recent Photos</h2>
+					<ul class="list-unstyled photo">
+						<li><a href="#" class="img" style="background-image: url(<?php echo base_url() . 'assets/images/1.jpg' ?>);"></a></li>
+						<li><a href="#" class="img" style="background-image: url(<?php echo base_url() . 'assets/images/2.jpg' ?>);"></a></li>
+						<li><a href="#" class="img" style="background-image: url(<?php echo base_url() . 'assets/images/3.jpg' ?>);"></a></li>
+						<li><a href="#" class="img" style="background-image: url(<?php echo base_url() . 'assets/images/4.jpg' ?>);"></a></li>
+						<li><a href="#" class="img" style="background-image: url(<?php echo base_url() . 'assets/images/5.jpg' ?>);"></a></li>
+						<li><a href="#" class="img" style="background-image: url(<?php echo base_url() . 'assets/images/6.jpg' ?>);"></a></li>
+					</ul>
+				</div>
+			</div>
+			<div class="col-md">
+				<div class="ftco-footer-widget mb-4">
+					<h2 class="ftco-heading-2">Archives</h2>
+					<ul class="list-unstyled categories">
+						<li><a href="#">November 2020 <span>(105)</span></a></li>
+						<li><a href="#">October 2020 <span>(212)</span></a></li>
+						<li><a href="#">September 2020 <span>(150)</span></a></li>
+						<li><a href="#">August 2020 <span>(100)</span></a></li>
+						<li><a href="#">July 2020 <span>(200)</span></a></li>
+					</ul>
+				</div>
+			</div>
+			<div class="col-md">
+				<div class="ftco-footer-widget mb-4">
+					<h2 class="ftco-heading-2">Have a Questions?</h2>
+					<div class="block-23 mb-3">
+						<ul>
+							<li><span class="icon icon-map-marker"></span><span class="text">Panjunan Rt 04 Rw 01, Sukodono-Sidoarjo, Jawa Timur 61258</span></li>
+							<li><a href="#"><span class="icon icon-phone"></span><span class="text">+6289650776455 </span></a></li>
+							<li><a href="#"><span class="icon icon-envelope"></span><span class="text">Mochchoiruddin99@gmail.com.com</span></a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-12">
+
+				<p>
+					<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+					Copyright &copy;<script>
+						document.write(new Date().getFullYear());
+					</script> All rights reserved | This template is made with <i class="icon-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
+					<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+				</p>
+			</div>
+		</div>
+	</div>
+</footer>
